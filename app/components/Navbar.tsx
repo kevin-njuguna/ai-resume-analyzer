ab4a19f5d16dc0f8e1f3393e3d3cdd14e7dc5243
kevin-njuguna <kevy.kamah@gmail.com>
2025-08-20 12:05:18 +0300

    add Upload Form page & File Uploader Component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,16 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return(
-        <nav className="navbar">
-            <Link to="/"><p className="text-2xl font-bold text-gradient">Resumind</p></Link>
-            <Link to="/upload"><p className="primary-button w-fit">Upload Resume</p></Link>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">Resumind</p>
+      </Link>
+      <Link to="/upload">
+        <p className="primary-button w-fit">Upload Resume</p>
+      </Link>
+    </nav>
+  );
+};
 
 export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import {resumes} from "../../constants";
+import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import {usePuterStore} from "~/lib/puter";
-import {useNavigate} from "react-router";
-import {useEffect} from "react";
-
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,29 +14,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const {auth} = usePuterStore()
-  const navigate = useNavigate()
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(!auth.isAuthenticated) navigate('/auth?next=/');
-  }, [auth.isAuthenticated])
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
-    <Navbar/>
-    <section className="main-section">
-      <div className="page-heading py-16">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback.</h2>
-      </div>
-      {resumes.length > 0 && (
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+        {resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume: Resume) => (
-                <ResumeCard key={resume.id} resume={resume}/>
+              <ResumeCard key={resume.id} resume={resume} />
             ))}
           </div>
-      )}
-
-
-    </section>
-  </main>
+        )}
+      </section>
+    </main>
+  );
 }
